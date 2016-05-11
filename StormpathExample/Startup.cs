e2222868d77109116f1b3a8d39def4fa8233bc5f
Nate Barbettini <nathanaelb@gmail.com>
2016-05-11 16:28:47 -0700

    Add intiial example

---
+++ StormpathExample/Startup.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="Startup.cs" company="Stormpath, Inc.">
+// Copyright (c) 2016 Stormpath, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using Microsoft.Owin;
+using Owin;
+using Stormpath.AspNet;
+using Stormpath.Configuration.Abstractions;
+
+[assembly: OwinStartup(typeof(StormpathExample.Startup))]
+
+namespace StormpathExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseStormpath(new StormpathConfiguration()
+            {
+                Application = new ApplicationConfiguration()
+                {
+                    Name = "My Application"
+                }
+            });
+        }
+    }
+}
